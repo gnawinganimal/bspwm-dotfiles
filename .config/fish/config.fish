f2307b2f188d0b865a566068ceb0cd1f216c6d45
gnawinganimal <unrongeur@proton.me>
2023-03-01 03:14:41 -0800

    Fishy fishy fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,24 +5,24 @@ end
 
 fish_add_path ~/.script
 fish_add_path ~/.local/bin
-fish_add_path ~/tizen-studio/package-manager
-fish_add_path /usr/local/i386elfgcc/bin/
+# fish_add_path ~/tizen-studio/package-manager
+# fish_add_path /usr/local/i386elfgcc/bin/
 
-alias sl='ls'
+# alias sl='ls'
 
-alias ls='lsd'
-alias cat='bat'
-alias fetch='gxrfetch'
-alias ping='gping'
-alias top='btop'
-alias curl='curlie'
+# alias ls='lsd'
+# alias cat='bat'
+# alias fetch='gxrfetch'
+# alias ping='gping'
+# alias top='btop'
+# alias curl='curlie'
 
-alias make='make -j4'
+# alias make='make -j4'
 
-alias ga='git add .'
-alias gc='git commit -m '
-alias gp='git push'
+# alias ga='git add .'
+# alias gc='git commit -m '
+# alias gp='git push'
 
-alias ma='micro *'
-alias m='micro'
-alias md='md-to-pdf'
+# alias ma='micro *'
+# alias m='micro'
+# alias md='md-to-pdf'
